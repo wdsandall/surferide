008a73d685868c285fb9bddeb73d3e194c7b37d7
William Sandall <wdsandall@gmail.com>
2017-02-04 15:17:55 +1100

    work in progress

--- test.py
+++ test.py
@@ -5,11 +5,18 @@ import pytz
 
 
 FIELDS = "&fields=solidRating,fadedRating,localTimestamp"
-URL = "http://magicseaweed.com/api/46b2f2f1d095df7f57d4d3a080731311/forecast/?spot_id="
-BEACH_ID =   {'Newquay': 1, 'Tallow': 1019, 'Byron Bay': 541, 'Bungan': 2928, 'Avalon': 2930}
 
-response = requests.get(URL + str(BEACH_ID['Tallow']) + FIELDS)
+URL = "http://magicseaweed.com/api/46b2f2f1d095df7f57d4d3a080731311/forecast/?spot_id="
+
+BEACH_ID =   {'Newquay': 1,
+              'Tallow': 1019,
+              'Byron Bay': 541,
+              'Bungan': 2928,
+              'Avalon': 2930}
+
+response = requests.get(URL + str(BEACH_ID['Newquay']) + FIELDS)
 data = response.json()
+# data is now a list of 40 dictionaries each with a solidRating, fadedRating and localTimestamp field
 
 utc = pytz.utc
 fmt = "%c"
@@ -17,13 +24,24 @@ def stamped(epoch_time):
     stamp = dt.fromtimestamp(epoch_time, utc)
     return stamp.strftime(fmt)
 
-for period in data:
-    if period['solidRating'] >= 1:
-        good_time = stamped(period['localTimestamp'])
-        good_stars = "*" * period['solidRating']
-        good_stripes = "-" * period['fadedRating']
-        print(good_time + "       " +  good_stars + good_stripes)
+def print_stars():
+
+    # print("Printing results for " + BEACH_ID)
+
+    for period in data:
+        if period['solidRating'] >= 1:
+            good_time = stamped(period['localTimestamp'])
+            good_stars = "*" * period['solidRating']
+            good_stripes = "-" * period['fadedRating']
+            print(good_time + "       " +  good_stars + good_stripes)
+        
+print_stars()
+
+'''
+def get_results():
+    for key in BEACH_ID:
+
+'''
         
 
-
-
+        
