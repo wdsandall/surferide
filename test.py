9838c5bacc658cfd9c3eff86d4e32f0fd4dfdc90
William Sandall <wdsandall@gmail.com>
2017-02-04 14:13:36 +1100

    datetime formatting added

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import requests
 import json
+import time
+from datetime import datetime as dt
+import pytz
 
-fields = "&fields=solidRating,fadedRating"
+
+fields = "&fields=solidRating,fadedRating,localTimestamp"
 
 response = requests.get("http://magicseaweed.com/api/46b2f2f1d095df7f57d4d3a080731311/forecast/?spot_id=1" + fields)
 
@@ -16,7 +20,22 @@ for i in data:
 '''
 
 data = response.json()
+good_periods = []
+utc = pytz.utc
+fmt = "%c"
 
 for period in data:
     print(("*" * period['solidRating'])+("-" * period['fadedRating']))
+
+for period in data:
+    if period['solidRating'] >= 3:
+        
+        #stamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(period['localTimestamp']))
+        stamp = dt.fromtimestamp(period['localTimestamp'], utc)
+        good_periods.append(stamp.strftime(fmt))
+
+        
+print(good_periods)
+
+
 
