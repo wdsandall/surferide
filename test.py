fe65599229ef3112da5f81328d31b445ee12d719
William Sandall <wdsandall@gmail.com>
2017-02-04 13:26:06 +1100

    prints rating to console

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
 import requests
 import json
 
-fields = "&fields=solidRating,fadedRating,localTimestamp"
+fields = "&fields=solidRating,fadedRating"
 
 response = requests.get("http://magicseaweed.com/api/46b2f2f1d095df7f57d4d3a080731311/forecast/?spot_id=1" + fields)
 
-print(response.status_code)
+#CHECK THE STATUS
+# print(response.status_code)
 
 data = response.json()
 
-print(data[0])
+# print(data[0]['solidRating'])
 
+'''
 for i in data:
     print i
+'''
+
+for period in data:
+    print(("*" * period['solidRating'])+("-" * period['fadedRating']))
 
